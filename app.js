eb77928c7a01cca92db5733e761390616286a7aa
gunay-rza <gunaylinim2013@mail.ru>
2024-10-01 15:24:18 +0400

    arraygunay

---
+++ app.js
@@ -0,0 +1,49 @@
+"use strict"
+
+/* task1
+Üç qrup yoldaşının adlarından ibarət massiv yaradın; Başqa bir qrup yoldaşının adını massivin sonuna əlavə edin; İlk adı silin; Massivi konsola çıxarın.
+const telebe = ['Aygun', 'Ayten', 'Nergiz'];
+telebe.push('Seid');
+telebe.shift();
+console.log(telebe);*/
+
+
+
+
+/* task 2
+ Üç addan ibarət massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin. Massivi konsola çıxarın.
+
+const adlar = ['Aygun', 'Ayten', 'Nergiz'];
+adlar[1] = 'Classified';
+console.log(adlar);*/
+
+
+
+
+/*task 3
+Massiv verilib. "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın. Massiv elementlərin düzün.
+let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+
+let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+for (let i = 0; i < arr.length; i++) {
+    console.log(`episode ${i + 4}`)};*/
+
+
+
+
+/*task4
+İstifadəçidən vergüllə ayrılmış beş ədədi daxil etməyi xahiş edin və onları massivdə saxlayın. İstifadəçidən beş ədəd elementi daxil etməyi tələb edən, onları massivə yazan və ən kiçik ədədi konsola çıxaran proqram yazın.*/
+
+
+
+
+/*Verilən mətn tip dəyərdəki vergülləri nöqtəli vergüllə əvəz edin. '32, 31, 34, 36, 31' mətni verilmişdir ,İçindəki vergülləri nöqtəli vergüllə əvəz edin.(Massivden istifadə ederek
+
+const a = '32,31,34,36,31';
+const arr = a.split(',');
+const b = arr.join(';');
+console.log(b);*/
+
+
+
+
